8e82cf1b12f2186e03b0b434b59be1f8df8e89dc
Juan Camilo Quiceno <juan_camilo_quiceno@hotmail.com>
2024-10-17 19:16:21 -0500

    Ajustes calculadora

--- ecocuenta-web/src/app/components/home/home.component.ts
+++ ecocuenta-web/src/app/components/home/home.component.ts
@@ -1,14 +1,21 @@
 import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import { MatGridListModule } from '@angular/material/grid-list';
 import { SharedModule } from '../../shared/shared.module';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ SharedModule],
+  imports: [SharedModule, MatGridListModule],
   encapsulation: ViewEncapsulation.None,
   changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  constructor(private router: Router) {}
+
+  navigateToCalculator() {
+    this.router.navigate(['/calculator']);
+  }
 }
